387a4af9aa0f5218468a4b197b9232f14d2ab375
ruacs17 <ruacs17@gmail.com>
2023-08-15 10:09:21 +0800

    with child process

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
 const mqtt = require("mqtt");
 const conn = require('./db');
-var client = mqtt.connect('mqtt://127.0.0.1');
+var client = mqtt.connect('mqtt://127.0.0.1:1883');
 
 let curdt = new Date();
 let year = curdt.getFullYear();
 console.log(curdt.getHours()+" "+curdt.getMinutes());
 //testssss
-// client.on("connect",function(){
-//     client.subscribe("exampl");
-//     console.log("Subscription OK!");
-// });
+client.on("connect",function(){
+    client.subscribe("exampl");
+    console.log("Subscription OK!");
+});
 
-// client.on("message",function(topic,message){
+client.on("message",function(topic,message){
+    // conn.execute("SELECT * FROM users WHERE user_id=? LIMIT 2",[message.toString()], function(err, result, fields){
+    //     if(err)throw err;
+    //     var lastnme = result[0].lname; 
 
-
-//     conn.execute("SELECT * FROM users WHERE user_id=? LIMIT 2",[message.toString()], function(err, result, fields){
-//         if(err)throw err;
-//         var lastnme = result[0].lname; 
-        
-//        // 
-
-//         conn.execute("SELECT * FROM users WHERE lname=?",[lastnme],function(err, result, fields){
-//             if(err)throw err;
-//             console.log(result.length)
-//             client.publish('respns',lastnme);
-//         });
-//     });
+    //     conn.execute("SELECT * FROM users WHERE lname=?",[lastnme],function(err, result, fields){
+    //         if(err)throw err;
+    //         console.log(result.length)
+    //         client.publish('respns',lastnme);
+    //     });
+    // });
     
-//     console.log(topic.toString()+" "+message.toString());
-// });
+    console.log(topic.toString()+" "+message.toString());
+});
 
